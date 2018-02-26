830e2dd37a21845af3a6ef28842b1f2a615386e9
Zhe Xian Lee (Ethan) <zxlee618@gmail.com>
2018-02-27 08:29:52 +1100

    Initial Commit

---
+++ TextEffects/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextEffects
+//
+//  Created by Zhe Xian Lee on 27/2/18.
+//  Copyright © 2018 Zhe Xian Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
